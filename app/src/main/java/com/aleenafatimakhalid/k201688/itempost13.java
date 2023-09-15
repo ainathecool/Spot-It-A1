bf3820f36c9be3b8e2547b578fbfea1957bd9686
Aleena <ainookhalid@gmail.com>
2023-09-15 19:34:32 +0500

    Submission Commit 2

---
+++ app/src/main/java/com/aleenafatimakhalid/k201688/itempost13.java
@@ -0,0 +1,14 @@
+package com.aleenafatimakhalid.k201688;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class itempost13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itempost13);
+    }
+}
---
+++ app/src/main/java/com/aleenafatimakhalid/k201688/search7.java
@@ -0,0 +1,14 @@
+package com.aleenafatimakhalid.k201688;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class search7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search7);
+    }
+}
