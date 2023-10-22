59c8cbb861b27a3ad0e24a7767fd574e6d35832b
Aleena Fatima Khalid <ainookhalid@gmail.com>
2023-10-22 19:25:01 +0500

    edit message implemented, almost working with 5 min timestamp, need to fix some hardcoding

--- app/src/main/java/com/aleenafatimakhalid/k201688/ChatMessageAdapter.java
+++ app/src/main/java/com/aleenafatimakhalid/k201688/ChatMessageAdapter.java
@@ -1,15 +1,17 @@
 package com.aleenafatimakhalid.k201688;
 
 import android.content.Context;
-import android.util.Log;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -36,7 +38,6 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.
         } else {
             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.received_message, parent, false);
         }
-        Log.d("FirebaseData", "viewtype: " + viewType );
         return new ViewHolder(view, viewType);
     }
 
@@ -45,7 +46,6 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.
         ChatMessageItem chatMessage = chatMessages.get(position);
         holder.messageTextView.setText(chatMessage.getMessage());
         holder.timestampTextView.setText(String.valueOf(chatMessage.getTimestamp()));
-        Log.d("FirebaseData", "messageDisplayed: " + holder.messageTextView );
 
         if (chatMessage.getMessageType() == ChatMessageItem.TYPE_SENT) {
             // If it's a sent message, add a click listener to the "deleteMessage" ImageView
@@ -56,32 +56,38 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.
                     deleteMessage(chatMessage);
                 }
             });
+
+            // Add a click listener to the "editMessage" ImageView
+            holder.editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Call a method to allow the user to edit the message
+                    editMessage(chatMessage);
+                }
+            });
         } else {
             // If it's not a sent message, remove any existing click listener (if any)
-//            holder.deleteButton.setOnClickListener(null);
         }
     }
 
     private void deleteMessage(final ChatMessageItem chatMessage) {
         // Get the generated ID of the message
         final String messageId = chatMessage.getMessageId();
-        final String message = chatMessage.getMessage();
 
         // Get a reference to your Firebase database
         DatabaseReference messagesDatabase = FirebaseDatabase.getInstance().getReference("ChatMessages");
 
-
         // Assuming you have a reference to the Firebase database
         String key = messagesDatabase.child("ChatItem1").getKey();
 
-        DatabaseReference messageRef = messagesDatabase.child("ChatItem").child(key);
+        DatabaseReference messageRef = messagesDatabase.child("ChatItem1").child(key);
+
         // Remove the message from the real-time database
         messageRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 // Message deleted from the database, now remove it from the adapter
                 chatMessages.remove(chatMessage);
-                Toast.makeText(context, "Message deleted successfully.", Toast.LENGTH_SHORT).show();
                 notifyDataSetChanged(); // Notify the adapter that the data set has changed
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -93,6 +99,69 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.
         });
     }
 
+    private void editMessage(final ChatMessageItem chatMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Edit Message");
+
+        // Create an EditText to allow the user to edit the message
+        final EditText editText = new EditText(context);
+        editText.setText(chatMessage.getMessage());
+        builder.setView(editText);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Get the edited message text
+                String editedMessage = editText.getText().toString();
+
+                // Update the message in the Firebase Realtime Database
+                updateMessageInDatabase(chatMessage, editedMessage);
+
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updateMessageInDatabase(final ChatMessageItem chatMessage, final String editedMessage) {
+        // Get a reference to your Firebase database
+        DatabaseReference messagesDatabase = FirebaseDatabase.getInstance().getReference("ChatMessages");
+
+        // Get the unique key of the message
+        //String key = messagesDatabase.child("ChatItem1").getKey();
+
+        String message = chatMessage.getMessage();
+        // Update the message in the real-time database
+        DatabaseReference messageRef = messagesDatabase.child("ChatItem1").child("-NhMjnx4OBxubMLvZ8Kh");
+        messageRef.child("message").setValue(editedMessage).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                // Message updated successfully in the database
+                Toast.makeText(context, "Message edited successfully.", Toast.LENGTH_SHORT).show();
+
+                // Update the message in the local list
+                chatMessage.setMessage(editedMessage);
+                notifyDataSetChanged(); // Notify the adapter that the data set has changed
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Handle any errors that occur during the update, e.g., show a toast message
+                Toast.makeText(context, "Message edit failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
     @Override
@@ -109,23 +178,21 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<ChatMessageAdapter.
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView messageTextView;
         TextView timestampTextView;
-
         ImageView deleteButton;
+        ImageView editButton;
+
         public ViewHolder(View itemView, int viewType) {
             super(itemView);
             messageTextView = itemView.findViewById(
                     viewType == ChatMessageItem.TYPE_SENT ? R.id.sentMessage : R.id.receivedMessage
             );
-             Log.d("FirebaseData", "itemtype: " + viewType );
-            Log.d("FirebaseData", "TextView: " + messageTextView );
             timestampTextView = itemView.findViewById(
                     viewType == ChatMessageItem.TYPE_SENT ? R.id.timestampSentMessage : R.id.receivedMessageTimestamp
             );
-            Log.d("FirebaseData", "timestamp: " + timestampTextView );
 
-            if(viewType == ChatMessageItem.TYPE_SENT)
-            {
+            if (viewType == ChatMessageItem.TYPE_SENT) {
                 deleteButton = itemView.findViewById(R.id.deleteMessage);
+                editButton = itemView.findViewById(R.id.editMessage);
             }
         }
     }
